d5565c6affcb51dcb436d2d287f043d23dae6623
yakovsmonarch <medvejonok07@gmail.com>
2022-07-08 10:44:59 +0800

    Create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parser_FlenovInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
